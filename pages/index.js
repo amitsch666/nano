27bb3950d83a219c4af887c11746e3757b696f30
Amit Schandillia <amit@peppyburro.com>
2017-07-21 18:22:01 +0000

    Added NextJS config

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,11 @@ export default () => (
     <Head>
       <title>My page title</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="stylesheet" href="/static/style.css" />
     </Head>
     <p>Hello world!</p>
     <button onClick={() => Router.push('/about')}>About</button>
-    <Link href="/about">
+    <Link prefetch href="/about">
       <a>About Page</a>
     </Link>
   </div>
