f92fb3e609a8b92794541daf780d87e798bb852a
Amit Schandillia <amit@peppyburro.com>
2017-07-21 18:23:36 +0000

    Removed .build from git watch

--- .build/bundles/pages/about.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([5],{335:function(e,t,l){e.exports=l(336)},336:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(10),a=n(u),o=l(72),r=n(o),d=l(51),c=n(d);t.default=function(){return a.default.createElement("div",null,a.default.createElement(r.default,null,a.default.createElement("title",null,"About us"),a.default.createElement("meta",{name:"viewport",content:"initial-scale=1.0, width=device-width"})),a.default.createElement("p",null,"Hello world!"),a.default.createElement("button",{onClick:function(){return c.default.push("/")}},"Home"))}}},[335]);
-            return { page: comp.default }
-          })
-        
