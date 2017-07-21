aeb0bf4469407e703bffc6308571bbe1d89a992b
Amit Schandillia <amit@peppyburro.com>
2017-07-21 21:08:19 +0000

    Added inline Sass

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Router from 'next/router'
 import Link from 'next/link'
 
+import stylesheet from '../styles/main.scss'
+
 export default () => (
   <div>
     <Head>
       <title>My page title</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="stylesheet" href="/static/style.css" />
+			<style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </Head>
     <p>Hello world!</p>
     <button onClick={() => Router.push('/about')}>About</button>
