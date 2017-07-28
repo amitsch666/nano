dd00639192dd3e70c76e08097ae0a3e7deebfbab
Amit Schandillia <amit@peppyburro.com>
2017-07-28 18:37:28 +0000

    Removed .shown from .collapse-nav-title

--- components/TopNav.js
+++ components/TopNav.js
@@ -38,7 +38,7 @@ export default class TopNav extends React.Component {
 						<span id="tale">Tale</span><span id="spinners">Spinners</span>
           </NavbarBrand>
           <Collapse isOpen={this.state.isOpen} navbar delay={{show: 0, hide: 350}}>
-						<div id="collapse-nav-head" className={this.state.isOpen ? `shown` : ``}>
+						<div id="collapse-nav-head">
 							<div id="collapse-nav-title" className="p-2">TaleSpinners</div>
 							<Button outline color="secondary" onClick={this.toggle} className="m-auto"><span aria-hidden="true">&times;</span></Button>
 						</div>
