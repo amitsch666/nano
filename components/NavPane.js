2d8716eadb87f554f23e9be0f404b20e3c789ee5
Amit Schandillia <amit@peppyburro.com>
2017-07-29 13:27:34 +0000

    Minor fixes to NavPane component.

--- components/NavPane.js
+++ components/NavPane.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Button, Collapse, Navbar, Nav, NavItem, NavLink } from 'reactstrap';
 
-const NavPane = props => {
-  return (
-    <Collapse isOpen={props.isOpen} navbar delay={{show: 0, hide: 350}}>
-      <div id="collapse-nav-head">
-        <div id="collapse-nav-title" className="p-2">TaleSpinners</div>
-        <Button outline color="secondary" onClick={props.evtToggler} className="m-auto"><span aria-hidden="true">&times;</span></Button>
-      </div>
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink href="/browse">Browse</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/community">Community</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/login">Sign In</NavLink>
-        </NavItem>
-      </Nav>
-    </Collapse>
-  )
-}
-export default NavPane
+export default (props) => (
+  <Collapse isOpen={props.isOpen} navbar delay={{show: 0, hide: 350}}>
+    <div id="collapse-nav-head">
+      <div id="collapse-nav-title" className="p-2">TaleSpinners</div>
+      <Button outline color="secondary" onClick={props.evtToggler} className="m-auto"><span aria-hidden="true">&times;</span></Button>
+    </div>
+    <Nav className="ml-auto" navbar>
+      <NavItem>
+        <NavLink href="/browse">Browse</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/community">Community</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/login">Sign In</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+)
