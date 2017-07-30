00d0e01bf49d46f4398f1e507277408400587118
Amit Schandillia <amit@peppyburro.com>
2017-07-30 12:14:02 +0000

    Implemented swipe-to-close on NavPane.

--- components/NavPane.js
+++ components/NavPane.js
@@ -5,15 +5,27 @@ import onClickOutside from 'react-onclickoutside'
 class NavPane extends React.Component {
   constructor(props) {
     super(props);
+    let lastX = 0;
   }
   handleClickOutside = evt => {
     if(this.props.isOpen){
       this.props.toggle()
     }
   }
+  _onTouchStart = e => {
+    this.lastX = e.touches[0].clientX
+  }
+  _onTouchMove = e => {
+    let currentX = e.touches[0].clientX;
+    // alert(`lastx=${this.lastX}, currentx=${currentX}`);
+    if(currentX > this.lastX + 10){
+      this.props.toggle()
+    }
+    this.lastX = currentX;
+  }
   render(){
     return(
-      <Collapse isOpen={this.props.isOpen} navbar delay={{show: 0, hide: 350}}>
+      <Collapse isOpen={this.props.isOpen} navbar delay={{show: 0, hide: 350}} onTouchStart={this._onTouchStart} onTouchMove={this._onTouchMove}>
         <div id="collapse-nav-head">
           <div id="collapse-nav-title" className="p-2">TaleSpinners</div>
           <Button outline color="secondary" onClick={this.props.toggle} className="m-auto"><span aria-hidden="true">&times;</span></Button>
