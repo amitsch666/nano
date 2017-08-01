57177bc3492046ac77349bd63f4fcf90b29a29a9
Amit Schandillia <amit@peppyburro.com>
2017-08-01 16:46:48 +0000

    Fixed linting issues in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,67 +1,67 @@
-const path = require('path')
-const glob = require('glob')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack')
-const dotenv = require('dotenv').config()
+const path = require('path');
+const glob = require('glob');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+require('dotenv').config();
 
 module.exports = {
-	distDir: '.build',
+  distDir: '.build',
   webpack: (config) => {
     config.module.rules.push(
       {
         test: /\.(css|scss)$/,
         loader: 'emit-file-loader',
         options: {
-					name: path.join('dist', '[path][name].[ext]')
-        }
+          name: path.join('dist', '[path][name].[ext]'),
+        },
       },
       {
         test: /\.(css|sass|scss)$/,
         use: ExtractTextPlugin.extract([
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: false,
-              minimize: true
-						}
-					},
-					'postcss-loader',
           {
-						loader: 'sass-loader',
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+              minimize: true,
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
             options: {
               includePaths: ['styles', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
+                .map(d => path.join(__dirname, d))
+                .map(g => glob.sync(g))
+                .reduce((a, c) => a.concat(c), []),
+            },
           },
-        ])
+        ]),
       },
-			{
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: 'url-loader?limit=10000&mimetype=application/font-woff&outputPath=static/'
-			},
-			{
-				test: /\.(svg|ttf|eot)$/i,
-				loader: 'file-loader?outputPath=static/'
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				loaders: [
-					'file-loader?outputPath=static/',
-					'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-				]
-			},
-    )
-		config.plugins.push(
-			new ExtractTextPlugin({
-				filename: path.join('static', 'main.css')
-			}),
-			new webpack.DefinePlugin({
-				'process.env.MY_NAME': JSON.stringify(process.env.MY_NAME),
-        'process.env.MY_CITY': JSON.stringify(process.env.MY_CITY)
-			})
-		)
-    return config
-  }
-}
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff&outputPath=static/',
+      },
+      {
+        test: /\.(svg|ttf|eot)$/i,
+        loader: 'file-loader?outputPath=static/',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loaders: [
+          'file-loader?outputPath=static/',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
+      },
+    );
+    config.plugins.push(
+      new ExtractTextPlugin({
+        filename: path.join('static', 'main.css'),
+      }),
+      new webpack.DefinePlugin({
+        'process.env.MY_NAME': JSON.stringify(process.env.MY_NAME),
+        'process.env.MY_CITY': JSON.stringify(process.env.MY_CITY),
+      }),
+    );
+    return config;
+  },
+};
